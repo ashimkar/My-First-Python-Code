bbe276c6898f352c811fc33632ac8c0f8da42cd7
ashimkar <150282433+ashimkar@users.noreply.github.com>
2023-11-09 17:58:36 +0600

    Add files via upload

---
+++ Variables 2.py
@@ -0,0 +1,11 @@
+first_name = 'ash'
+last_name = 'kar'
+full_name = first_name +' '+last_name
+print('hello '+full_name)
+print(type('hello '+full_name))
+
+age = 26
+age = age + 1
+print('your age is '+str(age))
+print(type('your age is '+str(age)))
+#print(type(age))
