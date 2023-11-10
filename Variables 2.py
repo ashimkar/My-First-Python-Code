5305a6be65b2679314e6d3d02392fcd8ac41e6a7
ashimkar <150282433+ashimkar@users.noreply.github.com>
2023-11-10 23:19:43 +0600

    Update Variables 2.py

--- Variables 2.py
+++ Variables 2.py
@@ -1,11 +1,34 @@
-first_name = 'ash'
-last_name = 'kar'
-full_name = first_name +' '+last_name
-print('hello '+full_name)
-print(type('hello '+full_name))
+first_name = "ashim"
+last_name = "kar"
+full_name = first_name + " " +last_name
+print("hello "+full_name)
+print(type("hello "+full_name))
 
 age = 26
-age = age + 1
-print('your age is '+str(age))
-print(type('your age is '+str(age)))
-#print(type(age))
+age = age + 1.
+print("your age is: "+str(age))
+print(type("your age is "+str(age)))
+
+height = 205.5
+height += 1
+print("your height is: "+ str(height))
+print("your name is: "+ full_name + " | " + "your height is: "+ str(height) + " | " + "and your age is: "+ str(age) + "cm")
+
+#name = "ashim"
+#age = 26
+#attractive = True
+name, age, attractive = "ashim", 26, True
+print(name)
+print(age)
+print(attractive)
+
+
+#ashim = 30
+#shifat = 30
+#hemel = 30
+#joy = 30
+ashim = shifat = hemel = joy = 30
+print(ashim)
+print(shifat)
+print(hemel)
+print(joy)
